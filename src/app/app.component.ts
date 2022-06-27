fcdda9bc524b21cc2123f9ebdd7a2b0ad709bb2c
feifeiliang <a0125994@u.nus.edu>
2022-06-27 15:55:33 +0800

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+const shajs = require('sha.js');
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  pAmount: number;
+
+  constructor(private http: HttpClient) {}
+
+  initialPayment(pAmount) {
+    console.log(pAmount);
+    this.http
+      .post<any>('https://open-sea.alipayplus.com/aps/api/v1/payments/pay', {
+        title: 'Angular POST Request Example',
+      })
+      .subscribe((data) => {});
+  }
 }
